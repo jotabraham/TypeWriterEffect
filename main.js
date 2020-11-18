d862c63fff8483bf26a9d06a9e485373bf909155
jotabraham <jotabraham@gmail.com>
2020-11-18 12:04:49 -0500

    beginning js

---
+++ main.js
@@ -0,0 +1,24 @@
+const TypeWriter = function(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+}
+
+// Type Method
+
+// Init on DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+
+    // Init TypeWriter
+    new TypeWriter(txtElement, words, wait);
+}
